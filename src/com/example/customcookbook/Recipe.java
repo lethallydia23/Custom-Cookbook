7d95a0d3b69adf1e44ea78a1f9b6108ab1c32c58
Lydia Buzzard <lydia.buzzard299@topper.wku.edu>
2015-03-20 21:39:07 -0500

    Finished basic Recipe Class

--- src/com/example/customcookbook/Recipe.java
+++ src/com/example/customcookbook/Recipe.java
@@ -1,4 +1,7 @@
 package com.example.customcookbook;
+
+import java.util.ArrayList;
+
 /*
  * Lydia Buzzard
  * March 20, 2015
@@ -9,5 +12,77 @@ package com.example.customcookbook;
  */
 public class Recipe 
 {
+	//Parameters
+	String name;
+	ArrayList<String> ingredients;
+	//Maybe add an image parameter to store photo of recipe?
+	StringBuffer steps;	//If we save the image, this param may be deleted
+	
+	//Default constructor
+	public Recipe()
+	{
+		//Initialize empty params
+		name="";
+		ingredients = new ArrayList<String>();
+		steps = new StringBuffer();
+		
+	}
+	
+	//Initial value constructor
+	public Recipe(String name)
+	{
+		this.name = name;
+		ingredients = new ArrayList<String>();
+		steps = new StringBuffer();
+	}
+	
+	//SET AND GET METHODS
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void addIngredient(String ingred)
+	{
+		ingredients.add(ingred);
+	}
+	
+	public void setSteps(StringBuffer steps)
+	{
+		this.steps  = steps;
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	public String getIngredient(int index)
+	{
+		return this.ingredients.get(index);
+	}
+	
+	public StringBuffer getSteps()
+	{
+		return this.steps;
+	}
+	
+	public String toString()
+	{
+		String result = "";
+		int counter = 0;
+		
+		result = String.format("%s:\n", name);
+		
+		for(counter = 0; counter < ingredients.size(); counter++)
+		{
+			result+=String.format("%d. %s\n", counter+1, ingredients.get(counter));
+		}
+		
+		result+=steps;
+		
+		return result;
+	}
 
 }
