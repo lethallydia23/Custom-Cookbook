67b1d66bba651c626afa5270440b196a57503d05
Lydia Buzzard <lydia.buzzard299@topper.wku.edu>
2015-03-20 21:20:37 -0500

    Added Project

---
+++ src/com/example/customcookbook/Recipe.java
@@ -0,0 +1,13 @@
+package com.example.customcookbook;
+/*
+ * Lydia Buzzard
+ * March 20, 2015
+ * 
+ * This class represents a Recipe object to be created within the MainActivity. Parameters include a recipe name, list of ingredients,
+ * and a StringBuffer of steps. The class contains default and initial value constructors along with basic set/get methods and a
+ * toString method for printing the Recipe.
+ */
+public class Recipe 
+{
+
+}
